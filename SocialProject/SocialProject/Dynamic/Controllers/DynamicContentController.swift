37b0b5415a4e0b9d9f420c76d34d4ce0021833bf
SexiZhang <416243653@qq.com>
2018-08-17 15:06:33 +0800

    收藏

--- SocialProject/SocialProject/Dynamic/Controllers/DynamicContentController.swift
+++ SocialProject/SocialProject/Dynamic/Controllers/DynamicContentController.swift
@@ -281,7 +281,7 @@ extension DynamicContentController {
     func collectRequest(indexPath: IndexPath) {
         self.showBlurHUD()
         var model = self.dataArray[indexPath.row]
-        let collectRequest = CollectRequest(ID: model.id, userLoginId: userID)
+        let collectRequest = CollectRequest(ID: userID, dynamicsId: model.id)
         WebAPI.send(collectRequest) { (isSuccess, result, error) in
             self.hideBlurHUD()
             if isSuccess {
